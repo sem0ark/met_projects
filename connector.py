c170faa4ea5deec88bacc4868205bf8ccc3929a5
sem0ark <arcadii.sem@gmail.com>
2022-12-14 08:52:43 +0200

    Sort imports

--- connector.py
+++ connector.py
@@ -12,10 +12,9 @@ The module implements solutions to the tasks:
 3. Query information from the API
 4. Process JSON data and return as object
 """
+import json
 
 import requests
-
-import json
 
 
 def req_json(req_url, headers=''):
@@ -66,6 +65,7 @@ def get_keys(filename='keys.txt'):
 #             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
 #         })
 #         return data
+
 
 class ACConnector:
     """
