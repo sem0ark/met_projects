0705679aed722bd84e3e8b1547e377ead415192e
sem0ark <arcadii.sem@gmail.com>
2023-01-13 11:53:25 +0100

    Add main.py module description

--- CS100 PZ01/main.py
+++ CS100 PZ01/main.py
@@ -1,3 +1,19 @@
+"""
+Main file for CS100 PZ01 application,
+group 2, topic 14
+
+Completed by Arkadii Senemov, 5833
+Software Engineering, year 1
+
+Program description:
+1. Functioning CLI that user can use to interact with the program.
+2. Retrieving the current data about air conditions for a general city
+    (searching functionality is included).
+3. Plotting data various way, so the user could analize the canges
+    in the state of the city.
+4. Printing a forecast data for some types of available polution values.
+"""
+
 from cli import CLI
 
 if __name__ == "__main__":
