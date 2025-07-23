41a6be50102985ae4ee7a86a3d670f05dd39501c
sem0ark <arcadii.sem@gmail.com>
2025-07-23 20:21:01 +0200

    feat: move configuration into models, add category model

---
+++ IT355-PZ01/src/main/java/com/it355/app/model/Category.java
@@ -0,0 +1,25 @@
+package com.it355.app.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+}
