70e525f53fc000c6566e16c0b7c98765fd641f8f
sem0ark <arcadii.sem@gmail.com>
2025-07-23 20:23:11 +0200

    feat: add initial category control logic

---
+++ IT355-PZ01/src/main/java/com/it355/app/controller/CategoryController.java
@@ -0,0 +1,58 @@
+package com.it355.app.controller;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.it355.app.model.*;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping("/")
+    public String listCategories(Model model) {
+        model.addAttribute("categories", categoryRepository.findAll());
+        return "category_list";
+    }
+
+    @GetMapping("/new")
+    public String newCategoryForm(Model model) {
+        model.addAttribute("category", new Category());
+        return "category_form";
+    }
+
+    @PostMapping("/save")
+    public String saveCategory(@ModelAttribute Category category) {
+        categoryRepository.save(category);
+        return "redirect:/categories/";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editCategoryForm(@PathVariable Integer id, Model model) {
+        model.addAttribute("category", categoryRepository.findById(id).orElse(null));
+        return "category_form";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteCategory(@PathVariable Integer id) {
+        Optional<Category> categoryOptional = categoryRepository.findById(id);
+        if (categoryOptional.isPresent()) {
+
+            categoryRepository.deleteById(id);
+        }
+        return "redirect:/categories/";
+    }
+}
