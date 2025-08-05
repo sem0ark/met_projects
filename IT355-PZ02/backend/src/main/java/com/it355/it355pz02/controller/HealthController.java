e97df7f277246bd3b665002de2a8f1e92a2a42bf
sem0ark <arcadii.sem@gmail.com>
2025-08-05 19:56:40 +0200

    feat: make initial full-stack configuration

---
+++ IT355-PZ02/backend/src/main/java/com/it355/it355pz02/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.it355.it355pz02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/ping")
+    public @ResponseBody String ping() {
+        return "pong";
+    }
+}
