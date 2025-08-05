860d957d6829b06ff37cb0a84884e1b09200ec12
sem0ark <arcadii.sem@gmail.com>
2025-08-05 19:33:14 +0200

    feat: it355 pz02 initial

---
+++ IT355-PZ02/backend/src/main/java/com/it355/it355pz02/It355pz02Application.java
@@ -0,0 +1,13 @@
+package com.it355.it355pz02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355pz02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It355pz02Application.class, args);
+	}
+
+}
---
+++ IT355-PZ02/backend/src/test/java/com/it355/it355pz02/It355pz02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it355.it355pz02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355pz02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
